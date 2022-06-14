1efb6fe658977d59cb12317a2c6bf0f0ab6191c8
norjamillekasan <88083841+norjamillekasan@users.noreply.github.com>
2022-06-14 10:45:28 +0800

    Update 2022_06_13_062855_create_customers_table.php

--- database/migrations/2022_06_13_062855_create_customers_table.php
+++ database/migrations/2022_06_13_062855_create_customers_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('full_name');
+            $table->string('full_name')->nullable();
             $table->string('selected_hours');
             $table->timestamps();
         });
